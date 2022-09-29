3259415f9eaa9822d88bc94ae411884c070f1ee7
Nicolas Alves <nfadevelop@gmail.com>
2022-09-29 00:12:37 -0300

    lower case model name

--- src/app/services/team.service.ts
+++ src/app/services/team.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import { Team } from '../models/team';
-import { GameeResponse } from '../base/models/gameeResponse';
 import { BaseService } from '../base/services/base.service';
 
 @Injectable()
